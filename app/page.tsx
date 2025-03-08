cb165591c99ad2ab4f66b144c806a0e011aa8d18
unknown <abhijitamdubey31@gmail.com>
2025-03-08 15:53:31 +0530

    Adding the sigib and sign up functunality

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
-
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 export default function Home() {
   return (
     <div >
-     hi their <b>tera bhai seedhe maut</b>
+     <Link href="/signin">Sign in</Link>
+     <br />
+     <Link href="/signup">Sign up</Link>
+     <br />
+     <button onClick={async () => {
+       const router = useRouter();
+       router.push("/signup");
+     }}>Fetch</button>
     </div>
   );
 }
